97a2928d367921fce16260320285eeed40034ecf
HoronLee <1269024821@qq.com>
2022-11-11 00:25:39 +0800

    Java工作室-自定义乘法表

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int c = str.charAt(0);
+        if (97 <= c && c < 123){
+            System.out.println("Ture");
+        }else {
+            System.out.println("False");
+        }
     }
 }
